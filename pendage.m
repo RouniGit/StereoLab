eeb4b04fb0cbe8c2159340c40d1a91263e018db9
Danny DOMINGUES <danny.domingues@outlook.fr>
2022-05-13 20:41:15 +0200

    Reformat comment

--- pendage.m
+++ pendage.m
@@ -1,4 +1,6 @@
 function p=pendage(dip,coord)
+%Permet de calculer les coordonnees après rotation d'angle dip
+
 rot_dip = [1,0,0;0, cosd(dip),sind(dip);0, -sind(dip), cosd(dip)];
 p = rot_dip*coord;
 end
